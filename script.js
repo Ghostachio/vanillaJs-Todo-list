b56bc9dcf7278cc232a2d0b6ac00a773a68be86d
Ghostachio <mansour.hamza2.0@gmail.com>
2025-08-27 22:57:12 +0100

    finished Basic Funtionnality of adding, deleteing, marking all done and clear tasks, added more basic styling and improved code readability

--- script.js
+++ script.js
@@ -1,24 +1,84 @@
 const task = document.querySelector("#task");
 const add = document.querySelector(".add");
 const list = document.querySelector(".todos-list");
+const isCheck = document.querySelector(".isCheck");
+const numTasks = document.querySelector("#numTasks");
+const clear = document.querySelector("#clear");
+const allDone = document.querySelector("#all-done");
+const markDone = document.querySelector(".markDone");
+const item = document.querySelector(".item");
 
-const todos = [];
+let todos = [];
+
+const renderTodos = () => {
+  list.innerHTML = "";
+  todos.forEach((el, i) => {
+    const check = document.createElement("input");
+    check.checked = el.isDone;
+    check.setAttribute("type", "checkbox");
+    const li = document.createElement("li");
+    const h3 = document.createElement("h3");
+    const deletebtn = document.createElement("span");
+    deletebtn.classList.add("material-symbols-outlined", "remove");
+    check.classList.add("isCheck");
+    deletebtn.innerText = "delete";
+    li.appendChild(check);
+    li.appendChild(h3);
+    li.appendChild(deletebtn);
+
+    h3.innerText = el.task;
+    list.appendChild(li);
+
+    check.addEventListener("change", () => {
+      el.isDone = check.checked;
+      h3.classList.toggle("done");
+      console.log(el.isDone);
+    });
+    deletebtn.addEventListener("click", () => {
+      li.remove();
+      removeTask(el.id);
+    });
+    if (el.isDone) {
+      h3.classList.add("done");
+    }
+  });
+
+  if (todos.length > 1) {
+    numTasks.innerText = `${todos.length} Tasks To Do`;
+  } else if (todos.length === 1) {
+    numTasks.innerText = "1 Task For Today  ";
+  } else {
+    numTasks.innerText = "No Tasks For Today yet ";
+  }
+};
 
 const addTodo = () => {
-  list.innerHTML = "";
-  const todo = { task: task.value, isDone: false };
+  const randomId = Math.floor(Math.random() * 1000);
+  if (!task.value) return;
+  const todo = { id: randomId, task: task.value, isDone: false };
   todos.push(todo);
-  todos.map((el, i) => {
-    const li = document.createElement("li");
-    li.innerText = el.task;
-    list.appendChild(li);
-  });
   task.value = "";
+  renderTodos();
+};
 
-  console.log(todos);
+const removeTask = (id) => {
+  todos = todos.filter((el) => el.id !== id);
+  renderTodos();
 };
 
 add.addEventListener("click", () => {
-  console.log(task.value);
   addTodo();
+});
+
+clear.addEventListener("click", () => {
+  todos = [];
+  renderTodos();
+});
+
+allDone.addEventListener("click", () => {
+  for (let todo of todos) {
+    todo.isDone = true;
+  }
+  console.log(todos);
+  renderTodos();
 });
